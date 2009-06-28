91502ba227784db84e7db458838aeb839ff1440c
Patrick Tierney <patrick.l.tierney@gmail.com>
2009-06-28 01:41:09 +0000

    Added callbacks to crypt-otr-member.c

--- crypt-otr-members.c
+++ crypt-otr-members.c
@@ -79,19 +79,13 @@ CV* crypt_otr_get_new_fpr_cb() { return crypt_otr_new_fpr_cb; }
 
 void crypt_otr_set_inject_cb( CryptOTRUserState in_state, CV* in_inject_cb ){ CRYPT_OTR_INSTALL_CALLBACK( in_state->inject_cb, in_inject_cb ); }
 
-void crypt_otr_set_system_message_cb( CryptOTRUserState in_state, CV* in_sys_mes_cb ){ crypt_otr_store_callback (in_state->system_message_cb, in_sys_mes_cb); }
-void crypt_otr_set_connected_cb( CryptOTRUserState in_state, CV* in_connected_cb ){ crypt_otr_store_callback( in_state->connected_cb, in_connected_cb); }
-void crypt_otr_set_unverified_cb( CryptOTRUserState in_state, CV* in_unver_cb ) { crypt_otr_store_callback( in_state->unverified_cb, in_unver_cb); }
-void crypt_otr_set_disconnected_cb( CryptOTRUserState in_state, CV* in_disconnected_cb ){ crypt_otr_store_callback( in_state->disconnected_cb, in_disconnected_cb); }
-void crypt_otr_set_stillconnected_cb( CryptOTRUserState in_state, CV* in_still_cb ) { crypt_otr_store_callback( in_state->stillconnected_cb, in_still_cb); }
-void crypt_otr_set_error_cb( CryptOTRUserState in_state, CV* in_error_cb ) { crypt_otr_store_callback( in_state->error_cb, in_error_cb); }
-void crypt_otr_set_warning_cb( CryptOTRUserState in_state, CV* in_warning_cb ) { crypt_otr_store_callback( in_state->warning_cb, in_warning_cb); }
-void crypt_otr_set_info_cb( CryptOTRUserState in_state, CV* in_info_cb ) { crypt_otr_store_callback( in_state->info_cb, in_info_cb); }
-void crypt_otr_set_new_fpr_cb( CryptOTRUserState in_state, CV* in_fpr_cb ) { crypt_otr_store_callback( in_state->new_fpr_cb, in_fpr_cb); }
-
-
-void crypt_otr_store_callback( CV* struct_callback, CV* perl_callback ){
-	SvREFCNT_inc(perl_callback);
-	struct_callback = perl_callback;
-}
+void crypt_otr_set_system_message_cb( CryptOTRUserState in_state, CV* in_sys_mes_cb ){ CRYPT_OTR_INSTALL_CALLBACK(in_state->system_message_cb, in_sys_mes_cb); }
+void crypt_otr_set_connected_cb( CryptOTRUserState in_state, CV* in_connected_cb ){ CRYPT_OTR_INSTALL_CALLBACK( in_state->connected_cb, in_connected_cb); }
+void crypt_otr_set_unverified_cb( CryptOTRUserState in_state, CV* in_unver_cb ) { CRYPT_OTR_INSTALL_CALLBACK( in_state->unverified_cb, in_unver_cb); }
+void crypt_otr_set_disconnected_cb( CryptOTRUserState in_state, CV* in_disconnected_cb ){ CRYPT_OTR_INSTALL_CALLBACK( in_state->disconnected_cb, in_disconnected_cb); }
+void crypt_otr_set_stillconnected_cb( CryptOTRUserState in_state, CV* in_still_cb ) { CRYPT_OTR_INSTALL_CALLBACK( in_state->stillconnected_cb, in_still_cb); }
+void crypt_otr_set_error_cb( CryptOTRUserState in_state, CV* in_error_cb ) { CRYPT_OTR_INSTALL_CALLBACK( in_state->error_cb, in_error_cb); }
+void crypt_otr_set_warning_cb( CryptOTRUserState in_state, CV* in_warning_cb ) { CRYPT_OTR_INSTALL_CALLBACK( in_state->warning_cb, in_warning_cb); }
+void crypt_otr_set_info_cb( CryptOTRUserState in_state, CV* in_info_cb ) { CRYPT_OTR_INSTALL_CALLBACK( in_state->info_cb, in_info_cb); }
+void crypt_otr_set_new_fpr_cb( CryptOTRUserState in_state, CV* in_fpr_cb ) { CRYPT_OTR_INSTALL_CALLBACK( in_state->new_fpr_cb, in_fpr_cb); }
 
